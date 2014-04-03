5c8c7f66d17fa707df368d77dfdc8387a984b7eb
soiferj <soiferj@umich.edu>
2014-04-02 18:23:25 -0700

    Implemented + added functions in Menu.cs

--- Xbox360Game1/Xbox360Game1/Menu.cs
+++ Xbox360Game1/Xbox360Game1/Menu.cs
@@ -15,6 +15,12 @@ namespace Xbox360Game1
     {
         List<String> menuOptions;
         String menuName;
+        int selectedIndex;
+
+        Color unselected = Color.WhiteSmoke;
+        Color selected = Color.Red;
+
+        SpriteFont font;
 
         /* Constructors (you can't use optional arguments in here...) */
         public Menu(List<String> menuOptions) : this(menuOptions, "Menu", null) {}
@@ -28,20 +34,75 @@ namespace Xbox360Game1
         }
         /**/
 
+        protected int SelectedIndex
+        {
+            get
+            {
+                return selectedIndex;
+            }
+
+            set
+            {
+                selectedIndex = value;
+            }
+        }
 
         public override void Update(GameTime gameTime, GraphicsDeviceManager graphics)
         {
-            throw new NotImplementedException();
+            gamePadState = GamePad.GetState(PlayerIndex.One);
+
+            if (gamePadState.IsButtonDown(Buttons.DPadDown) || gamePadState.IsButtonDown(Buttons.LeftThumbstickDown))
+            {
+                selectedIndex++;
+                if (selectedIndex == menuOptions.Count())
+                    selectedIndex = 0;
+            }
+
+            if (gamePadState.IsButtonDown(Buttons.DPadUp) || gamePadState.IsButtonDown(Buttons.LeftThumbstickUp))
+            {
+                selectedIndex--;
+                if (selectedIndex < 0)
+                    selectedIndex = menuOptions.Count() - 1;
+            }
         }
 
         public override void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content)
         {
-            throw new NotImplementedException();
+            // nothing to load unless we use textures
+            return;
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            throw new NotImplementedException();
+            Vector2 location = position;
+            Color tint;
+
+            for (int i = 0; i < menuOptions.Count(); i++)
+            {
+                if (i == selectedIndex)
+                    tint = selected;
+                else
+                    tint = unselected;
+
+                spriteBatch.DrawString(font, menuOptions[i], location, tint);
+                location.Y += font.LineSpacing + 5;
+            }
+        }
+
+        private void MeasureMenu()
+        {
+            float height = 0;
+            float width = 0;
+
+            foreach (String option in menuOptions)
+            {
+                Vector2 stringSize = font.MeasureString(option);
+                if (stringSize.X > width)
+                    width = stringSize.X;
+                height += font.LineSpacing + 5;
+            }
+
+            position = new Vector2((screenBounds.Width - width) / 2, (screenBounds.Height - height) / 2);
         }
     }
 }
